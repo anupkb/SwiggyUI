24ce541b36ffa5eab9d330e315c9d6db83497aef
ANUP KB <anupkb01@gmail.com>
2024-04-11 17:14:18 +0530

    For routing: react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React from "react";
 import { ReactDOM, createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import Contact from "./components/Contact";
+import About from "./components/About";
 
 const App = () => {
   return (
     <>
       <Header />
-      {/* <Body resData={resData} /> */}
       <Body />
     </>
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
+
 const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+// root.render(<App />);
+root.render(<RouterProvider router={appRouter} />);
